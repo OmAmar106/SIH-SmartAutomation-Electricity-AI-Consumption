eb381b2a20b7b27a663c0e001cf6967364b17f5d
OmAmar <bt23cse106@iiitn.ac.in>
2024-11-27 00:37:26 +0530

    vercel hosting

--- main/app.py
+++ main/app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from flask import Flask, redirect, render_template, request, jsonify,url_for
 from flask import session
 from table import *
@@ -15,7 +18,11 @@ from tmodel import *
 app = Flask(__name__)
 app.config['CACHE_TYPE'] = 'simple'
 cache = Cache(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.sqlite3"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join('/tmp', 'database.sqlite3')}"
+db_path = '/tmp/database.sqlite3'
+if not os.path.exists(db_path):
+    open(db_path, 'w').close()
+os.chmod(db_path, 0o777)
 db.init_app(app)
 app.app_context().push()
 app.secret_key = "APtlnuRu04uv"
